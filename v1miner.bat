ef366909d76a4235424b104c53c83c7565185240
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-09-21 21:22:07 +0300

    Add files via upload

---
+++ v1miner.bat
@@ -0,0 +1,1 @@
+node v1miner.js
---
+++ v1miner.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+console.log('Welcome to Probot Miner !')
+console.log('DONT USE THIS CODE ON YOUR REAL ACCOUNT !') // WARNING !
+
+client.on('ready', async() => {
+    const server = "492747482942406656"; // ايدي السررفر
+const channel = "492747482942406659";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send(`**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز
+    كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز
+    كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز  
+    كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز
+    **`)//spamming message.
+},1);//Delay Time
+})
+client.on('ready', async() => {
+    const server = "492747482942406656"; // ايدي السررفر
+const channel = "492747482942406659";//ايدي الروم
+        setInterval(()=>{
+        client.guilds.get(server).channels.get(channel).send(`*ssssssssssssssssssssdadasdadasidjapoidnadipkandsaidjha0piwdawnidkkkkkdkdkdkdkdkdkmsaddaopidwdmaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawsakllmdsa;kkkkkdmsdk;samnkcnaksdnaaaaaaaaaasjfakjandjakdnakl
+        **`)//spamming message.
+    },305);//Delay Time
+    })
+
+client.login("NDI5OTcyMDMwMDkyNDc2NDM3.DoagHQ.h_F0VOXDMSLFgnVF7PJ7ymgpc0Q")//Your Token
+
+  
